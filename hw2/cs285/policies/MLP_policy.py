49ed6a11655529ec0f6da244888a31096a7ce105
Suraj Narayanan Sasikumar <surajx@gmail.com>
2020-12-21 13:14:25 +1100

    Implemented stuff from hw1

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -89,7 +89,13 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from hw1
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        action_distribution = self.forward(ptu.from_numpy(observation))
+        action = ptu.to_numpy(action_distribution.sample())
         return action
 
     # update/train this policy
@@ -102,7 +108,14 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor):
-        # TODO: get this from hw1
+        if self.discrete:
+            logits = self.logits_na(observation)
+            action_distribution = torch.distributions.Categorical(logits=logits)
+        else:
+            mean = self.mean_net(observation)
+            std = torch.exp(self.logstd)
+            action_distribution = torch.distributions.Normal(loc=mean, scale=std)
+
         return action_distribution
 
 
